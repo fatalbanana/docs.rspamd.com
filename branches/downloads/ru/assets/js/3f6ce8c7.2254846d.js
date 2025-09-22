cd4400b0a9dc3538b073c759ddb189c4f553784a
fatalbanana <nerf@judo.za.org>
2025-09-22 15:26:16 +0000

    Deploying to gh-pages from @ fatalbanana/docs.rspamd.com@3f18d329e81b2bc01bf714590bfc9c5fa9ac5a15 🚀

---
+++ branches/downloads/ru/assets/js/3f6ce8c7.2254846d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrspamd_docs=self.webpackChunkrspamd_docs||[]).push([[5468],{436:s=>{s.exports=JSON.parse('{"tags":[{"label":"performance","permalink":"/docs.rspamd.com/branches/downloads/ru/blog/tags/performance","count":1},{"label":"benchmarks","permalink":"/docs.rspamd.com/branches/downloads/ru/blog/tags/benchmarks","count":1}]}')}}]);
